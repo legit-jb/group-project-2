9791132492261bd7147da1a306372f9a3aa3d5bd
Dawan Sawyer <dawansawyer@gmail.com>
2021-04-17 13:53:36 -0400

    created route for homepage to get movies and also created login route, as well as saving the loggedin session

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,35 +1,43 @@
 const router = require("express").Router();
-const { User } = require("../models");
-const withAuth = require("../utils/auth");
+const { Movie, Movielist } = require("../models");
 
-// Prevent non logged in users from viewing the homepage
-router.get("/", withAuth, async (req, res) => {
+
+// GET all movies for homepage
+router.get('/', async (req, res) => {
   try {
-    const userData = await User.findAll({
-      attributes: { exclude: ["password"] },
-      order: [["name", "ASC"]],
+    const dbMoviesData = await Movie.findAll({
+      include: [
+        {
+          model: Movielist,
+          attributes: ['movie_id'],
+        },
+      ],
     });
 
-    const users = userData.map((project) => project.get({ plain: true }));
-
-    res.render("homepage", {
-      users,
-      // Pass the logged in flag to the template
-      logged_in: req.session.logged_in,
+    const movies = dbMoviesData.map((movies) =>
+      movies.get({ plain: true })
+    );
+    // Send over the 'loggedIn' session variable to the 'homepage' template
+    res.render('homepage', {
+      movies,
+      loggedIn: req.session.loggedIn,
     });
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
 
-router.get("/login", (req, res) => {
-  // If a session exists, redirect the request to the homepage
-  if (req.session.logged_in) {
-    res.redirect("/");
+
+// Login route
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect to the homepage
+  if (req.session.loggedIn) {
+    res.redirect('/');
     return;
   }
-
-  res.render("login");
+  // Otherwise, render the 'login' template
+  res.render('login');
 });
 
 module.exports = router;
