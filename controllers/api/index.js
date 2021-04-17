49e4823a8c981e0e645fde2040c969402f81a8d2
Dawan Sawyer <dawansawyer@gmail.com>
2021-04-17 16:09:03 -0400

    fixed association problem

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,6 +3,8 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const listRoutes = require('./list-routes');
 
+const movieRoutes = require('./movie-routes');
+router.use('/movies', movieRoutes)
 router.use('/users', userRoutes);
 router.use('/lists', listRoutes);
 
