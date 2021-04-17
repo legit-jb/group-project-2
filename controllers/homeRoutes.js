140ec50e64f3af7fad19af67dad47261c8b6e24b
Dawan Sawyer <dawansawyer@gmail.com>
2021-04-17 14:18:33 -0400

    created route for homepage to get movies from list and send logged in session

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,24 +1,21 @@
 const router = require("express").Router();
 const { Movie, Movielist } = require("../models");
 
-
 // GET all movies for homepage
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const dbMoviesData = await Movie.findAll({
       include: [
         {
           model: Movielist,
-          attributes: ['movie_id'],
+          attributes: ["movie_id"],
         },
       ],
     });
 
-    const movies = dbMoviesData.map((movies) =>
-      movies.get({ plain: true })
-    );
+    const movies = dbMoviesData.map((movies) => movies.get({ plain: true }));
     // Send over the 'loggedIn' session variable to the 'homepage' template
-    res.render('homepage', {
+    res.render("homepage", {
       movies,
       loggedIn: req.session.loggedIn,
     });
@@ -28,16 +25,15 @@ router.get('/', async (req, res) => {
   }
 });
 
-
 // Login route
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   // If the user is already logged in, redirect to the homepage
   if (req.session.loggedIn) {
-    res.redirect('/');
+    res.redirect("/");
     return;
   }
   // Otherwise, render the 'login' template
-  res.render('login');
+  res.render("login");
 });
 
 module.exports = router;
