16ddc25cd49f5a56c284ab0fcc8114847c4ad69b
Sean Kelly <invertedrdm@gmail.com>
2021-04-18 12:08:32 -0400

    added some data to homeroutes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,15 +1,33 @@
 const router = require("express").Router();
-const Movie = require("../models");
+const { Movie, User, List, Movielist } = require('../models');
 const withAuth = require("../utils/auth");
 
-router.get('/', function (req, res) {
-  res.render('homepage')
-})
+// gets user data and connects it to the handlebars homepage
+router.get('/', async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const listData = await List.findAll({
+      include: [
+        {
+          model: Movie, through: Movielist
+        },
+      ],
+    });
 
-router.get('/login', function (req, res) {
-  res.render('login')
+    // Serialize data so the template can read it
+    const lists = listData.map((list) => list.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      lists, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+// If user is logged in redirects to homepage, otherwise renders login page
 router.get('/login', (req, res) => {
   // If a session exists, redirect the request to the homepage
   if (req.session.logged_in) {
@@ -17,14 +35,6 @@ router.get('/login', (req, res) => {
     return;
   }
   res.render('login');
-});
-
-// create logout
-
-
-
-router.get('/profile', function (req, res) {
-  res.render('profile')
 });
 
 module.exports = router;
