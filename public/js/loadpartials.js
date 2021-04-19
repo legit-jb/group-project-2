2c286ae20593f79211f8132a8c06ec5b0b15e1fd
Artemis Morrow <c.artemis.morrow@gmail.com>
2021-04-19 13:51:09 -0400

    movie modal mostly working

--- public/js/loadpartials.js
+++ public/js/loadpartials.js
@@ -18,3 +18,28 @@ newList.addEventListener('click', () => {
 
   listForm.style.display = 'block'
 })
+
+async function newFormHandler(event) {
+  event.preventDefault();
+  const list_name = document.querySelector('#new-list-name').value;
+  
+  
+  const response = await fetch(`/api/list`, {
+    method: 'POST',
+    body: JSON.stringify({
+      list_name
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  
+  if (response.ok) {
+      listForm.style.display = 'none'
+      listBlock.style.display = 'block'
+  } else {
+    alert('Failed to add list');
+  }
+}
+
+document.querySelector('.new-list-form').addEventListener('submit', newFormHandler);
