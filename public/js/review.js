9aade71f4c9016e70643ca081ede85c39b399812
Dawan Sawyer <dawansawyer@gmail.com>
2021-04-19 14:53:33 -0400

    created comment functionality

--- public/js/review.js
+++ public/js/review.js
@@ -18,6 +18,19 @@ const commentFormHandler = async (event) => {
   }
 };
 
+const upvote = document.getElementById("upvote");
+const downvote = document.getElementById("downvote");
+
+upvote.addEventListener("click", () => {
+    upvote.style.color = '#f2e5d5';
+})
+
+downvote.addEventListener("click", () => {
+    downvote.style.color = '#f2e5d5';
+})
+
 document
   .querySelector(".comment-form")
   .addEventListener("submit", signupFormHandler);
+
+  
